ab28a14d1cac3c4e98340c9336963a4c318244ea
Mikhail Cheremuhin-Rerberg <misha1247@mail.ru>
2025-03-14 11:25:28 +0300

    feat(*): expand max length of track strings

--- src/models/track.ts
+++ src/models/track.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const TrackSchema = new mongoose.Schema({
-  name: { type: String, required: true, maxlength: 50 },
-  author: { type: String, required: true, maxlength: 50 },
+  name: { type: String, required: true, maxlength: 150 },
+  author: { type: String, required: true, maxlength: 150 },
   releaseDate: { type: String, required: true },
-  genre: { type: String, required: true, maxlength: 50 },
+  genre: { type: String, required: true, maxlength: 150 },
   durationInSeconds: { type: Number, required: true },
-  album: { type: String, maxlength: 50 },
+  album: { type: String, maxlength: 150 },
   previewUrl: { type: String, required: false },
   trackUrl: { type: String, required: true },
 });
